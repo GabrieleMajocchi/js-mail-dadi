8b0bc603d6d568ffc0ae4c67d345a6a337cf1c09
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-05-17 17:00:20 +0200

    made the random dice thrower with player vs pc

---
+++ js/myscript.js
@@ -0,0 +1,20 @@
+
+// Calcolo randomico dadi
+
+console.log('-Lancio dadi-')
+
+const PlayerDice = Math.floor(Math.random() * 6) + 1
+console.log('Hai fatto: ' + PlayerDice)
+
+const AiDice = Math.floor(Math.random() * 6) + 1
+console.log('Il pc ha fatto: ' + AiDice)
+
+Check del giocatore col numero più alto
+
+if (PlayerDice > AiDice){
+    console.log('Hai vinto!')
+}else if(AiDice > PlayerDice){
+    console.log('Ha vinto il pc')
+}else{
+    console.log('Spareggio')
+}
